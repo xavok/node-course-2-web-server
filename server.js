e824bb5df602cb8e8dda716c125be318883a9d57
Xavok <xavoking@gmail.com>
2017-04-30 22:06:59 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+/**
+ * Created by Xavok on 4/30/2017.
+ */
+"use strict";
+
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const fs = require('fs');
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+// app.use((req,res,next) => {
+//     res.render('maintenance');
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+
+app.get('/', (req,res) => {
+    res.render('home', {
+        title: 'Home Page',
+        welcomeMessage: 'Welcome text',
+    });
+});
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About Page',
+    });
+});
+
+app.get('/bad',(req,res) => {
+    res.send({
+        errorMessage: 'Error'
+    })
+});
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
