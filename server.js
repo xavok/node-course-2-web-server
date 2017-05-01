179ccc2ae4266d3774cca9ec5109af743e924580
Xavok <xavoking@gmail.com>
2017-04-30 22:25:03 -0700

    Setup starts script and heroku port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 app.use((req,res,next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
@@ -54,6 +54,6 @@ app.get('/bad',(req,res) => {
         errorMessage: 'Error'
     })
 });
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
